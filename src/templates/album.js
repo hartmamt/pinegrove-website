873ed6648f5000ebd752c3f6d685e8390dc5f44b
Matthew Hartman <mattandalissa@gmail.com>
2019-07-11 08:51:50 -0400

    genius test

--- src/templates/album.js
+++ src/templates/album.js
@@ -107,9 +107,6 @@ export const pageQuery = graphql`
           ...GatsbyContentfulFluid
         }
       }
-      tracklisting {
-        json
-      }
       tracks {
         songTitle
         audioFile {
